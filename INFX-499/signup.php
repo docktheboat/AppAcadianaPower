ff5da8b5dd0bbc39653da41a8cb20b107eef754f
Peyton Revere <34105378+Peytzn@users.noreply.github.com>
2022-03-15 00:36:41 -0500

    Add files via upload

---
+++ INFX-499/signup.php
@@ -0,0 +1,17 @@
+<body>
+<main id="main-holder">
+    <h1 id="login-header">Sign Up</h1>
+
+    <form id="login-form">
+        <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
+        <p></p>
+        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Re-enter Password">
+        <p></p>
+
+    </form>
+
+    <input type="submit" value="Sign Up" id="login-form-submit">
+
+</main>
+</body>
