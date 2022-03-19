5e0e014b5124277885748f0e2c485aa99567c9fe
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-19 15:58:03 -0500

    Adding a weather widget on dashboard to display weather data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core'
+import { WeatherService } from '../weather/weather.service'
+import { OnInit } from '@angular/core'
+import { Weather } from '../weather/weather'
+import { HttpErrorResponse } from '@angular/common/http'
 
 @Component({
     templateUrl: 'user.component.html',
     styleUrls: ['./user.component.css']
 })
-export class UserComponent {}
+export class UserComponent implements OnInit{
+    public weatherInfo : Weather[] | undefined
+    public iconClass : String | undefined
+
+    constructor(private weatherService: WeatherService){}
+
+    ngOnInit(): void {
+        this.getWeather();
+    }
+
+    public getWeather(){
+        this.weatherService.getWeather().subscribe(
+            (response : Weather) => {
+                this.iconClass = this.weatherService.getIcon(response.conditions);
+                this.weatherInfo = [response];
+            },
+            (error: HttpErrorResponse) => {
+                alert(error.message);
+            }
+        )
+    }
+}
