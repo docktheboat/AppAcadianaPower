9118afe83fc836b6dfaf4b42c4c8e0904060aef8
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-19 15:56:52 -0500

    Adding a link to manage customers on customer page

--- src/app/outage/outage.component.ts
+++ src/app/outage/outage.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-root',
+ // selector: 'app-root',
   templateUrl: './outage.component.html',
   styleUrls: ['./outage.component.css']
 })
