82c2b97099c2fb48cf94b25bf74679aaa0432818
docktheboat <patrickedgarcourts@yahoo.com>
2022-03-27 10:03:06 -0500

    Removing files that are not needed in main dir

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "shared.php";
-    top();
-    ?>
-<div>
-    <button class="buttonGreen" role="button" onclick="window.location.href='login.php';">Login</button>
-    <button class="buttonGreen" role="button" onclick="window.location.href='register.php';">Sign Up</button>
-</div>
-    <?php bottom(); ?>
